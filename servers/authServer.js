3abdf88e9c3ae28bdca2bb9a52e440d47956a69e
Stefan Tsankov <stsankov@shopmetrics.com>
2020-02-06 17:42:56 +0200

    FB authentication

--- servers/authServer.js
+++ servers/authServer.js
@@ -1,9 +1,25 @@
 require('dotenv').config()
 
+
+
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 
+
+
+const 
+    passport          =     require('passport')
+  , FacebookStrategy  =     require('passport-facebook').Strategy
+  , session           =     require('express-session')
+  , cookieParser      =     require('cookie-parser')
+  , bodyParser        =     require('body-parser')
+  , config            =     require('./configFBPassport')
+  //, mysql             =     require('mysql');
+
+
+
+console.log('config---',config)
 app.use(express.json())
 
 let refreshTokens = []
@@ -34,7 +50,7 @@ const posts = [
 
 
 app.get('/', function(req, res) {
-  res.sendfile('index.html');
+  res.sendfile('servers/index.html');
 });
  
 
@@ -98,5 +114,121 @@ app.post('/login', (req, res) => {
 function generateAccessToken(user) {
   return jwt.sign( { name: user.name ,mutatePayload:true}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10000' })
 }
+
+// ---------------------------- FB Login implementation 
+
+passport.use(new FacebookStrategy({
+  clientID: config.facebook_api_key,
+  clientSecret:config.facebook_api_secret ,
+  callbackURL: config.callback_url
+},
+function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function () {
+    //Check whether the User exists or not using profile.id
+    if(config.use_database) {
+       //Further code of Database.
+    }
+    return done(null, profile);
+  });
+}
+));
+
+
+
+//Define MySQL parameter in Config.js file.
+// const pool = mysql.createPool({
+//   host     : config.host,
+//   user     : config.username,
+//   password : config.password,
+//   database : config.database
+// });
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FacebookStrategy within Passport.
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_api_key,
+    clientSecret:config.facebook_api_secret ,
+    callbackURL: config.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      console.log(accessToken)
+      console.log(refreshToken)
+      console.log(profile)
+
+      if(config.use_database) {
+        // if sets to true
+        // pool.query("SELECT * from user_info where user_id="+profile.id, (err,rows) => {
+        //   if(err) throw err;
+        //   if(rows && rows.length === 0) {
+        //       console.log("There is no such user, adding now");
+        //       pool.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+        //   } else {
+        //       console.log("User already exists in database");
+        //   }
+        // });
+      }
+      return done(null, profile);
+    });
+  }
+));
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ 
+  secret: 'keyboard cat', 
+  key: 'sid',
+  resave: true,
+  saveInitialized: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//app.use(express.static(__dirname + '/public'));
+
+//app.get('/', function(req, res){
+ // res.render('index', { user: req.user });
+//});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect : '/', failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('redirectg ? ')
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+
 
 app.listen(4000)
---
+++ servers/configFBPassport.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "facebook_api_key"      :     "803258203418735",
+    "facebook_api_secret"   :     "592108eb8f0d10f677526c0f71698688",
+    "callback_url"          :     "http://localhost:4000/auth/facebook/callback",
+    "use_database"          :      false,
+    "host"                  :     "localhost",
+    "username"              :     "root",
+    "password"              :     "",
+    "database"              :     "Database Name"
+  }
+
